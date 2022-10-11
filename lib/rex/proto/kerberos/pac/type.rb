1604c2d81ada197f1112b8c6f45246985e3a675f
Christophe De La Fuente <christophe_delafuente@rapid7.com>
2022-10-11 11:33:10 +0200

    Update PacType to decode from string
    - Use BinData
    - Add PacInfoBuffer structure

--- lib/rex/proto/kerberos/pac/type.rb
+++ lib/rex/proto/kerberos/pac/type.rb
@@ -18,6 +18,105 @@ module Rex
           # Key used to create the checksum
           attr_accessor :key
 
+          class PacInfoBuffer < BinData::Record
+            endian :little
+
+            uint32 :ul_type
+            uint32 :cb_buffer_size
+            uint64 :offset
+          end
+
+          class PacType < BinData::Record
+            endian :little
+
+            uint32 :c_buffers, initial_value: -> { buffers.size }
+            uint32 :version
+            array  :info_buffers, type: :pac_info_buffer, read_until: -> { index == c_buffers - 1 }
+            array  :raw_buffer, type: :uint8, read_until: :eof
+
+            def buffers
+              result = []
+              info_buffers.each do |info_buffer|
+                offset = info_buffer.offset - raw_buffer.abs_offset
+                case info_buffer.ul_type
+                when PAC_CLIENT_INFO
+                  # TODO
+                  #result << ClientInfo.decode(raw_buffer[offset, info_buffer.cb_buffer_size)
+                when PAC_LOGON_INFO
+                  # TODO
+                  #result << LogonInfo.decode(raw_buffer[offset, info_buffer.cb_buffer_size)
+                when PAC_PRIVSVR_CHECKSUM
+                  # TODO
+                  #result << PrivSvrChecksum.decode(raw_buffer[offset, info_buffer.cb_buffer_size)
+                when PAC_SERVER_CHECKSUM
+                  # TODO
+                  #result << ServerChecksum.decode(raw_buffer[offset, info_buffer.cb_buffer_size)
+                when PAC_CREDENTIALS_INFO
+                  credential_info = CredentialInfo.new
+                  result << credential_info.decode(raw_buffer.to_binary_s[offset, info_buffer.cb_buffer_size])
+                end
+              end
+
+              result
+            end
+          end
+
+          # Decodes the Rex::Proto::Kerberos::Pac::Type from an input
+          #
+          # @param input [String] the input to decode from
+          # @return [self] if decoding succeeds
+          # @raise [Rex::Proto::Kerberos::Model::Error::KerberosDecodingError] if decoding doesn't succeed
+          def decode(input)
+            pac_type = PacType.read(input)
+            self.buffers = pac_type.buffers
+            self
+          end
+
+          # Decodes the buffers_length from a string
+          #
+          # @param input [String] the input to decode from
+          def decode_buffers_length(input)
+            input.unpack1('V')
+          end
+
+          # Decodes the version from a string
+          #
+          # @param input [String] the input to decode from
+          def decode_version(input)
+            input.unpack1('V')
+          end
+
+          # Decodes the PAC_INFO_BUFFER's from a string
+          #
+          # @param input [String] the input to decode from
+          def decode_pac_info_buffers(input)
+            offset = 8 + buffers.length * 16
+            encoded = ''
+            buffers.each do |buffer|
+              case buffer
+              when ClientInfo
+                encoded << [PAC_CLIENT_INFO].pack('V')
+              when LogonInfo
+                encoded << [PAC_LOGON_INFO].pack('V')
+              when PrivSvrChecksum
+                encoded << [PAC_PRIVSVR_CHECKSUM].pack('V')
+              when ServerChecksum
+                encoded << [PAC_SERVER_CHECKSUM].pack('V')
+              when CredentialInfo
+                encoded << [PAC_CREDENTIALS_INFO].pack('V')
+              end
+
+              buffer_length = buffer.encode.length
+
+              encoded << [buffer_length].pack('V')
+              encoded << [offset].pack('Q<')
+
+              offset = (offset + buffer_length + 7) / 8 * 8
+            end
+
+            encoded
+          end
+
           # Encodes the Rex::Proto::Kerberos::Pac::Type
           #
           # @return [String]
@@ -88,6 +187,8 @@ module Rex
                 encoded << [PAC_PRIVSVR_CHECKSUM].pack('V')
               when ServerChecksum
                 encoded << [PAC_SERVER_CHECKSUM].pack('V')
+              when CredentialInfo
+                encoded << [PAC_CREDENTIALS_INFO].pack('V')
               end
 
               buffer_length = buffer.encode.length
